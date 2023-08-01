06855a6ab07f5c2630f0ddcc68dfcb0f6d9f8a20
Roman Kachniuk <roman.kachniuk@gmail.com>
2023-08-01 23:08:42 +0200

    added class WordCount

--- src/NumberGame/Main.java
+++ src/NumberGame/Main.java
@@ -1,3 +1,5 @@
+package NumberGame;
+
 import java.util.Scanner;
 
 public class Main {
---
+++ src/WordCount/WordCount.java
@@ -0,0 +1,13 @@
+package WordCount;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class WordCount {
+    public static void main(String[] args) throws Exception {
+        File file = new File("98.rtf");
+        Scanner scanner = new Scanner(file);
+
+
+    }
+}
