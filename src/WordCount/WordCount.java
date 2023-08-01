1ffab3cbb1967d6834d946ced6a8ed1e03c81933
Roman Kachniuk <roman.kachniuk@gmail.com>
2023-08-01 23:14:08 +0200

    complete code in class WordCount

--- src/WordCount/WordCount.java
+++ src/WordCount/WordCount.java
@@ -7,7 +7,12 @@ public class WordCount {
     public static void main(String[] args) throws Exception {
         File file = new File("98.rtf");
         Scanner scanner = new Scanner(file);
-
+        int word = 0;
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            word += line.split(" ").length;
+        }
+        System.out.println("The file contains: " + word + " words");
 
     }
 }
